d538dcacc57842526f871568bcd1d74ef6bb4eba
Nimit <nimitagg95@gmail.com>
2020-01-13 21:26:48 +0530

    about heading
    Signed-off-by: Nimit <nimitagg95@gmail.com>

--- src/About.jsx
+++ src/About.jsx
@@ -28,6 +28,13 @@ const useStyle = makeStyles(theme => ({
     width: "250px",
 
     display: "block"
+  },
+  heading: {
+    color: "#66fcf1",
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "300",
+    fontSize: "30px",
   }
 }));
 
@@ -36,26 +43,34 @@ function About(props) {
   return (
     <div className={classes.root}>
       <Grid container direction="row" justify="space-between" alignItems="center">
-        <Grid item xs={0.5}><AnimatedLeftIcon onClick={() => {props.history.replace("/")}}/></Grid>
-        <Grid item container direction="column" justify="center" alignItems="center" xs={10}>
-          <Grid container item direction="row" justify="space-evenly" alignItems="center" xs={9}>
-            <Grid item xs={3}>
-              <div>
-                <img className={classes.centerCropped}
-                     src={"https://avatars0.githubusercontent.com/u/14093420?s=460&v=4"}
-                     alt="recipe thumbnail"/>
-              </div>
-            </Grid>
-            <Grid item xs={6}>
-              {data.aboutText.split("\n").map(paraTxt => {
-                return <Typography paragraph className={classes.aboutTxt}>
-                  {paraTxt}
-                </Typography>
-              })}
-            </Grid>
+        <Grid item xs={0.5}>
+          <AnimatedLeftIcon onClick={() => {
+            props.history.replace("/")
+          }}/>
+        </Grid>
+
+        <Grid container item xs={10} alignItems="center" justify="space-evenly" direction="row">
+          <Grid item>
+            <div>
+              <img className={classes.centerCropped}
+                   src={"https://avatars0.githubusercontent.com/u/14093420?s=460&v=4"}
+                   alt="recipe thumbnail"/>
+            </div>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="h3" gutterBottom className={classes.heading}>
+              About Me!
+            </Typography>
+            {data.aboutText.split("\n").map(paraTxt => {
+              return <Typography paragraph className={classes.aboutTxt}>
+                {paraTxt}
+              </Typography>
+            })}
           </Grid>
         </Grid>
-        <Grid item  xs={0.5}> <AnimatedRightIcon onClick={() => {props.history.push("/experience")}}/></Grid>
+        <Grid item xs={0.5}> <AnimatedRightIcon onClick={() => {
+          props.history.push("/experience")
+        }}/></Grid>
       </Grid>
     </div>
   )
