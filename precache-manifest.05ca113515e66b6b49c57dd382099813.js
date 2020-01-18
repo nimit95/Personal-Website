206be7f3e733bcbf5c932a32633ed3bf2a05666c
Nimit <nimitagg95@gmail.com>
2020-01-18 21:19:23 +0530

    Updates

---
+++ precache-manifest.05ca113515e66b6b49c57dd382099813.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b72dfd103b865dd49e661d2824c45a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba1b983ad412736251ca",
+    "url": "/static/css/main.09caec3f.chunk.css"
+  },
+  {
+    "revision": "5a5975fa47398f5339d4",
+    "url": "/static/js/2.9b052311.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.9b052311.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba1b983ad412736251ca",
+    "url": "/static/js/main.c506c045.chunk.js"
+  },
+  {
+    "revision": "3877348275e8a43c71af",
+    "url": "/static/js/runtime-main.9270ea39.js"
+  }
+]);
