f35aa4fcfd133d5a24eab908ff9df464bcd9b2f8
Nimit <nimitagg95@gmail.com>
2020-01-18 21:57:59 +0530

    Updates

---
+++ precache-manifest.d6737c82faa536f8e49d4ed8bd8a9fc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7debebb1abf85d971cb430618e141d69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75569f0f0d423481622e",
+    "url": "/static/css/main.09caec3f.chunk.css"
+  },
+  {
+    "revision": "11183e594481744305f9",
+    "url": "/static/js/2.5bf07ace.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.5bf07ace.chunk.js.LICENSE"
+  },
+  {
+    "revision": "75569f0f0d423481622e",
+    "url": "/static/js/main.0dbacbc6.chunk.js"
+  },
+  {
+    "revision": "3877348275e8a43c71af",
+    "url": "/static/js/runtime-main.9270ea39.js"
+  }
+]);
